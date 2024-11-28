454fcd78c038c64e34419a22a860991fe78a57c7
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-28 14:39:15 +0100

    feat: bonus 1 complete

--- app.js
+++ app.js
@@ -5,3 +5,11 @@
 console.log("Hello Boolean");
 console.log(process.argv);
 const parola = process.argv[2];
+
+//1 BONUS
+const numero = parseInt(process.argv[2]);
+if (numero % 2 === 0) {
+    console.log("Pari");
+} else {
+    console.log("Dispari");
+}
