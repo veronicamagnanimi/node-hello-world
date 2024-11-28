3e8c569e328d8dbc9c2caa7ac91eb4e059e13c51
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-28 14:30:41 +0100

    feat: exercise complete

---
+++ app.js
@@ -0,0 +1,7 @@
+// const parola = process.argv[2];
+// const message = `Hello ${parola}`;
+// console.log(message);
+
+console.log("Hello Boolean");
+console.log(process.argv);
+const parola = process.argv[2];
